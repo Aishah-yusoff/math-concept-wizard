b043b072f343e444f57c06ecf31a947b808b7be2
Aishah-yusoff <nuraishah_yusoff@moe.edu.sg>
2025-03-19 15:04:22 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MathQuestionBot from "./MathQuestionBot";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <MathQuestionBot />
+  </React.StrictMode>
+);
